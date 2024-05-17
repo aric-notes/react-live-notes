541bbe0936a658e4fc245a3cee21e1adf8d3083a
aric <1290657123@qq.com>
2024-05-17 22:46:55 +0800

    feat: add scope

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
-import reactLogo from './assets/react.svg';
-import { LiveProvider, LiveEditor, LivePreview } from 'react-live';
-const code = `
-<h3 style={{
-  background: 'darkslateblue',
-  color: 'white',
-  padding: 8,
-  borderRadius: 4
-}}>
-  Hello World! 👋
-</h3>
-`;
+import { LiveProvider, LiveEditor, LivePreview, LiveError } from 'react-live';
+import code from './code.tsx?raw';
+import React, { useState, useEffect, useRef, useReducer } from 'react';
 
 function App() {
   return (
-    <div className="mx-auto mt-20 w-1/2 flex-col bg-slate-200 p-10 y-2">
-      <div className="text-center">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={reactLogo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <LiveProvider code={code}>
-        <div className="grid grid-cols-2 gap-4">
-          <LiveEditor className="font-mono" />
-          <LivePreview />
+    <div className="mx-auto mt-20 w-1/2 flex-col rounded-md bg-slate-200 p-5 shadow-md y-2">
+      <h1>Live Code Editor</h1>
+      <LiveProvider
+        code={code}
+        scope={{ React, useState, useEffect, useRef, useReducer }}>
+        <div className="y-2">
+          <LiveError className="text-red-600" />
+          <div className="grid grid-cols-2 gap-4">
+            <LiveEditor className="font-mono" />
+            <LivePreview />
+          </div>
         </div>
       </LiveProvider>
     </div>
---
+++ src/code.tsx
@@ -0,0 +1,9 @@
+<h3
+  style={{
+    background: 'darkslateblue',
+    color: 'white',
+    padding: 8,
+    borderRadius: 4
+  }}>
+  Hello World! 👋
+</h3>;
