88d9de060fd862cda0ba56f253b736769236e6e7
aric <1290657123@qq.com>
2024-05-18 10:29:41 +0800

    feat: docs

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [react()],
   server: {
     host: '0.0.0.0'
+  },
+  // set output dir to docs
+  build: {
+    outDir: 'docs'
   }
 });
